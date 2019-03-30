348256418cea06dedbedc7d82c1485d242624407
ayoub <hamoudi.ayoub@gmail.com>
2019-03-30 16:52:15 +0100

    sweet alert 2

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-content',
@@ -7,19 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
   
+  image = "https://avatars0.githubusercontent.com/u/6206647?s=460&v=4";
   editable = false;
+
   course = {
     id: 0,
     title: '',
     price: 0,
     description: ''
   }
+
   courses = [
     {id: 1, title: "Symfony", price: 12.578, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, aliquam."},
-    {id: 2, title: "Laravel", price: 45.554, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, aliquam."},
-    {id: 3, title: "Spring Boot", price: 25.123, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, aliquam."},
-    {id: 4, title: "Angular", price: 14.785, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, aliquam."},
-    {id: 5, title: "NodeJS", price: 8.2658, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, aliquam."},
+    {id: 2, title: "Laravel", price: 45.554, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, aliquam."}
   ];
 
   constructor() { }
@@ -30,16 +31,35 @@ export class ContentComponent implements OnInit {
 
   addCourse() {
     this.courses.unshift(this.course);
-    this.course = {
-      id: 0,
-      title: '',
-      price: 0,
-      description: ''
-    }
+    this.initCourse();
   }
 
   deleteCourse(index) {
-   this.courses.splice(index, 1);
+    
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this imaginary file!',
+      type: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+
+        this.courses.splice(index, 1);
+
+        Swal.fire({
+          title: 'Course is Deleted !',
+          text: 'description',
+          type: 'success',
+          timer: 5000
+        })
+      // For more information about handling dismissals please visit
+      // https://sweetalert2.github.io/#handling-dismissals
+      } 
+     
+    })
+
   }
 
   editCourse(course) {
@@ -48,14 +68,18 @@ export class ContentComponent implements OnInit {
   }
 
   updateCourse() {
+    
+    this.initCourse();
+    this.editable = false
+  }
+
+  initCourse() {
     this.course = {
       id: 0,
       title: '',
       price: 0,
       description: ''
     }
-
-    this.editable = false
   }
 
 }
